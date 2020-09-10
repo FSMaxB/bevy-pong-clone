123dc1d2e2c2f923aa137435e397bbb7884efd8d
Max Bruckner <max@maxbruckner.de>
2020-09-10 22:45:55 +0200

    Populate the world with people and greet them

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build()
-        .add_system(hello_world.system())
-        .run();
+	App::build()
+		.add_startup_system(add_people_to_world.system())
+		.add_system(hello_world.system())
+		.add_system(greet_people.system())
+		.run();
+}
+
+struct Person;
+struct Name(String);
+
+fn add_people_to_world(mut commands: Commands) {
+	commands
+		.spawn((Person, Name("Alice".into())))
+		.spawn((Person, Name("Bob".into())))
+		.spawn((Person, Name("Charlie".into())));
+}
+
+fn greet_people(_people: &Person, Name(name): &Name) {
+	println!("Hello {}!", name);
 }
 
 fn hello_world() {
-    println!("hello world!");
+	println!("hello world!");
 }
