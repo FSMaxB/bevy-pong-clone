098410fd1474b73c76d171e4a062860ed49eab01
Max Bruckner <max@maxbruckner.de>
2020-09-12 11:09:07 +0200

    Add ball movement

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,21 @@ fn main() {
 	App::build()
 		.add_default_plugins()
 		.add_startup_system(setup.system())
+		.add_system(ball_movement_system.system())
 		.run();
+}
+
+struct Ball {
+	velocity: Vec2,
+}
+
+impl Default for Ball {
+	fn default() -> Self {
+		const DEFAULT_VELOCITY: f32 = 100.0;
+		Self {
+			velocity: DEFAULT_VELOCITY * Vec2::new(1.0, 1.0).normalize(),
+		}
+	}
 }
 
 fn setup(mut commands: Commands) {
@@ -15,12 +29,20 @@ fn setup(mut commands: Commands) {
 fn spawn_ball(commands: &mut Commands) {
 	const SIZE: f32 = 50.0;
 
-	commands.spawn(SpriteComponents {
-		sprite: Sprite {
-			size: Vec2::new(SIZE, SIZE),
-		},
-		translation: Translation::new(300.0, 200.0, 0.0),
-		rotation: Rotation::from_rotation_z(std::f32::consts::PI / 4.0),
-		..Default::default()
-	});
+	commands
+		.spawn(SpriteComponents {
+			sprite: Sprite {
+				size: Vec2::new(SIZE, SIZE),
+			},
+			rotation: Rotation::from_rotation_z(std::f32::consts::PI / 4.0),
+			..Default::default()
+		})
+		.with(Ball::default());
+}
+
+fn ball_movement_system(time: Res<Time>, mut query: Query<(&Ball, &mut Translation)>) {
+	let time_delta = time.delta_seconds;
+	for (ball, mut translation) in &mut query.iter() {
+		translation.0 += time_delta * ball.velocity.extend(0.0);
+	}
 }
