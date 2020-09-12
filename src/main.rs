f145a3b0e1a8374f0715dffeb3100229ce3e9065
Max Bruckner <max@maxbruckner.de>
2020-09-12 11:19:17 +0200

    Add paddles

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ struct Ball {
 	velocity: Vec2,
 }
 
+struct Paddle;
+
 impl Default for Ball {
 	fn default() -> Self {
 		const DEFAULT_VELOCITY: f32 = 100.0;
@@ -24,6 +26,8 @@ impl Default for Ball {
 fn setup(mut commands: Commands) {
 	commands.spawn(Camera2dComponents::default());
 	spawn_ball(&mut commands);
+	spawn_paddle(&mut commands, Vec2::new(-300.0, 0.0));
+	spawn_paddle(&mut commands, Vec2::new(300.0, 0.0));
 }
 
 fn spawn_ball(commands: &mut Commands) {
@@ -38,6 +42,18 @@ fn spawn_ball(commands: &mut Commands) {
 			..Default::default()
 		})
 		.with(Ball::default());
+}
+
+fn spawn_paddle(commands: &mut Commands, start_position: Vec2) {
+	commands
+		.spawn(SpriteComponents {
+			sprite: Sprite {
+				size: Vec2::new(20.0, 200.0),
+			},
+			translation: Translation(start_position.extend(0.0)),
+			..Default::default()
+		})
+		.with(Paddle);
 }
 
 fn ball_movement_system(time: Res<Time>, mut query: Query<(&Ball, &mut Translation)>) {
