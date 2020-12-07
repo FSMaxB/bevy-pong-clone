365e05f0e75e0717108c14b6b28b8a4a4c622982
Max Bruckner <max@maxbruckner.de>
2020-12-07 21:56:37 +0100

    Refactor Ball

---
+++ src/ball.rs
@@ -0,0 +1,88 @@
+use crate::Collider;
+use bevy::core::Time;
+use bevy::ecs::{Query, Res};
+use bevy::math::{Vec2, Vec3};
+use bevy::sprite::collide_aabb::collide;
+use bevy::sprite::collide_aabb::Collision;
+use bevy::sprite::Sprite;
+use bevy::transform::components::Transform;
+use bevy::window::WindowResized;
+
+pub struct Ball {
+	speed: f32,
+	direction: Vec2,
+}
+
+impl Ball {
+	pub fn velocity(&self) -> Vec2 {
+		self.speed * self.direction.normalize()
+	}
+
+	pub fn update_after_window_resize(
+		&mut self,
+		resize_event: &WindowResized,
+		size: &mut Vec2,
+		translation: &mut Vec3,
+	) {
+		let window_height = resize_event.height as f32;
+		self.speed = window_height / 1.5;
+
+		let ball_width = 0.05 * window_height;
+		*size = Vec2::new(ball_width, ball_width);
+
+		*translation = Vec3::default();
+	}
+}
+
+impl Default for Ball {
+	fn default() -> Self {
+		Self {
+			speed: Default::default(),
+			direction: Vec2::new(1.0, 1.0).normalize(),
+		}
+	}
+}
+
+pub fn ball_movement_system(time: Res<Time>, mut query: Query<(&Ball, &mut Transform)>) {
+	let time_delta = time.delta_seconds;
+	for (ball, mut transform) in query.iter_mut() {
+		transform.translation += time_delta * ball.velocity().extend(0.0);
+	}
+}
+
+pub fn ball_collision_system(
+	mut ball_query: Query<(&mut Ball, &Transform, &Sprite)>,
+	collider_query: Query<(&Collider, &Transform, &Sprite)>,
+) {
+	for (mut ball, ball_transform, ball_sprite) in ball_query.iter_mut() {
+		for (_collider, collider_transform, collider_sprite) in collider_query.iter() {
+			let collision = collide(
+				ball_transform.translation,
+				ball_sprite.size,
+				collider_transform.translation,
+				collider_sprite.size,
+			);
+
+			let collision = match collision {
+				Some(collision) => collision,
+				None => continue,
+			};
+
+			use Collision::*;
+			let (reflect_x, reflect_y) = match collision {
+				Left => (ball.direction.x() > 0.0, false),
+				Right => (ball.direction.x() < 0.0, false),
+				Top => (false, ball.direction.y() < 0.0),
+				Bottom => (false, ball.direction.y() > 0.0),
+			};
+
+			if reflect_x {
+				*ball.direction.x_mut() = -ball.direction.x();
+			}
+
+			if reflect_y {
+				*ball.direction.y_mut() = -ball.direction.y();
+			}
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use crate::ball::Ball;
+use crate::ball::{ball_collision_system, ball_movement_system};
 use crate::paddle::paddle_movement_system;
 use crate::paddle::Paddle;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, PrintDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
-use bevy::sprite::collide_aabb::{collide, Collision};
 use bevy::window::WindowResized;
 
+mod ball;
 mod paddle;
 
 fn main() {
@@ -19,37 +21,6 @@ fn main() {
 		.add_system(window_resize_listener.system())
 		.add_system(ball_collision_system.system())
 		.run();
-}
-
-struct Ball {
-	speed: f32,
-	direction: Vec2,
-}
-
-impl Ball {
-	fn velocity(&self) -> Vec2 {
-		self.speed * self.direction.normalize()
-	}
-
-	fn ball_size_and_translation(&self, window_height: usize) -> (Vec2, Vec3) {
-		let ball_width = 0.05 * (window_height as f32);
-		let size = Vec2::new(ball_width, ball_width);
-		let translation = Vec3::default();
-		(size, translation)
-	}
-
-	fn update_after_window_resize(&mut self, window_height: usize) {
-		self.speed = (window_height as f32) / 1.5;
-	}
-}
-
-impl Default for Ball {
-	fn default() -> Self {
-		Self {
-			speed: Default::default(),
-			direction: Vec2::new(1.0, 1.0).normalize(),
-		}
-	}
 }
 
 #[derive(Clone, Copy)]
@@ -76,7 +47,7 @@ impl Player {
 	}
 }
 
-struct Collider;
+pub struct Collider;
 
 fn setup(mut commands: Commands) {
 	commands.spawn(Camera2dComponents::default());
@@ -127,49 +98,6 @@ fn spawn_paddle(commands: &mut Commands, player: Player) {
 		.with(Collider);
 }
 
-fn ball_movement_system(time: Res<Time>, mut query: Query<(&Ball, &mut Transform)>) {
-	let time_delta = time.delta_seconds;
-	for (ball, mut transform) in query.iter_mut() {
-		transform.translation += time_delta * ball.velocity().extend(0.0);
-	}
-}
-
-fn ball_collision_system(
-	mut ball_query: Query<(&mut Ball, &Transform, &Sprite)>,
-	collider_query: Query<(&Collider, &Transform, &Sprite)>,
-) {
-	for (mut ball, ball_transform, ball_sprite) in ball_query.iter_mut() {
-		for (_collider, collider_transform, collider_sprite) in collider_query.iter() {
-			let collision = collide(
-				ball_transform.translation,
-				ball_sprite.size,
-				collider_transform.translation,
-				collider_sprite.size,
-			);
-
-			let collision = match collision {
-				Some(collision) => collision,
-				None => continue,
-			};
-
-			use Collision::*;
-			let (reflect_x, reflect_y) = match collision {
-				Left => (ball.direction.x() > 0.0, false),
-				Right => (ball.direction.x() < 0.0, false),
-				Top => (false, ball.direction.y() < 0.0),
-				Bottom => (false, ball.direction.y() > 0.0),
-			};
-
-			if reflect_x {
-				*ball.direction.x_mut() = -ball.direction.x();
-			}
-
-			if reflect_y {
-				*ball.direction.y_mut() = -ball.direction.y();
-			}
-		}
-	}
-}
 #[derive(Default)]
 struct WindowResizeListenerState {
 	event_reader: EventReader<WindowResized>,
@@ -191,10 +119,7 @@ fn window_resize_listener(
 		}
 
 		for (mut ball, mut sprite, mut transform) in ball.iter_mut() {
-			let (size, translation) = ball.ball_size_and_translation(height);
-			sprite.size = size;
-			transform.translation = translation;
-			ball.update_after_window_resize(height);
+			ball.update_after_window_resize(resize_event, &mut sprite.size, &mut transform.translation);
 		}
 	}
 }
